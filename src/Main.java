914ce9365111b69426d69759cea590e71bf485e3
Hafizur Rahman <sohel.hafizurrahman@gmail.com>
2024-02-17 13:59:58 +0600

    2nd day

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
-        /*int[] num = new int[5];
-        String[] name = new String[5];
-        float[] marks = new float[5];*/
-
         Student[]students=new Student[5];
 
         Student hafiz=new Student();
